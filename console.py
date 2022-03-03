b704a03c5d31a60a97e55dca6f1f42db1eadd514
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-03 13:42:42 +0100

    Modified show and destroy

--- console.py
+++ console.py
@@ -31,6 +31,7 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         if arg in self.class_exist.keys():
             obj = self.class_exist[arg]
+            print(self.__str__)
         elif len(arg) == 0:
             print("** class name missing **")
             return False
@@ -43,13 +44,12 @@ class HBNBCommand(cmd.Cmd):
         elif arg[1] is not BaseModel or arg[1] is not BaseModel.__subclasses__
             print("** class doesn't exist **")
             return False
-        else:
-            print(self.__str__)
 
-    def destroy(self, arg):
+    def do_destroy(self, arg):
         if arg in self.class_exist.keys():
             obj = self.class_exist[arg]
             obj.load()
+            self.__del__()
         elif len(arg) == 0:
             print("** class name missing **")
             return False
