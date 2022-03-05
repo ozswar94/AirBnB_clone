a3336c8231ec33a73184757edb5138cd4d1ab2bd
Jordan Mpounza <3630@holbertonschool.com>
2022-03-05 14:54:51 +0000

    pycodestyle test

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ define Class HBNBCommand """
 
+
 import cmd
 import sys
 from models.engine.file_storage import FileStorage
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Unittest of Base class"""
 
+
 import unittest
 from datetime import datetime
 import time
